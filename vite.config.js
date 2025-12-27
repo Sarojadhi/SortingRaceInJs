ee6c8e6462ed47038985f6acfd05cf908b385e78
saroj adhi <saroj@saroj.saroj-pc>
2025-12-27 20:19:24 +0545

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -13,5 +11,5 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
-  base: "/SortingRaceInJs",
+  base: "/SortingRaceInJs/",
 })
